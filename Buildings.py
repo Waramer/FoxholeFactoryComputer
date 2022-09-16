dd9f4797a40b78e3ac5ac77fa83e03f129172322
Delezenne Quentin <quentin.delezenne@orange.fr>
2022-09-16 23:43:57 +0200

    First Version Working with interaction

--- Buildings.py
+++ Buildings.py
@@ -1,3 +1,63 @@
+# FACTORIES
 #[Usage,timer,[inputs],[PowerNeeded],[outputs],duration]
+class OilWell:
+    def __init__(self):
+        upgrade = input("\nYou are creating an OilWell, which upgrade do you wish? (0:None, 1:ElectricalOilWell, 2:Fracker) ")
+        self.processes = []
+        pp0=[False,0,[],0,[['Oil',1]],50]
+        if usageFactory(pp0) : self.processes.append(pp0)
+        if upgrade=='1':
+            pp1 = [False,0,[],2,[['Oil',1]],26]
+            if usageFactory(pp1) : self.processes.append(pp1)
+            pp2 = [False,0,[],2,[['LOil',75]],40]
+            if usageFactory(pp2) : self.processes.append(pp2) 
+        elif upgrade=='2':
+            pp3 = [False,0,[['LWater',25]],3,[['Oil',2]],40]
+            if usageFactory(pp3) : self.processes.append(pp3)
+            pp4 = [False,0,[['LWater',25]],3,[['LOil',75]],30]
+            if usageFactory(pp4) : self.processes.append(pp4)
 
+class CoalRefinery:
+    def __init__(self):
+        upgrade = input("\nYou are creating an CoalRafinery, which upgrade do you wish ? (0:None, 1:CokeFurnace, 2:CoalLiquifier, 3:AdvancedCoalLiquifier) ")
+        self.processes = []
+        pp0 = [False,0,[['Coal',200]],3,[['Coke',180]],120]
+        if usageFactory(pp0) : self.processes.append(pp0)
+        if upgrade=='1':
+            pp1 = [False,0,[['Coal',200]],3,[['Coke',165],['Sulfur',15]],120]
+            if usageFactory(pp1) : self.processes.append(pp1)
+        elif upgrade=='2':
+            pp2 = [False,0,[['Coal',300],['LWater',75]],4,[['Concrete',180],['LOil',50]],120]
+            if usageFactory(pp2) : self.processes.append(pp2)
+        elif upgrade=='3':
+            pp3 = [False,0,[['Coal',300],['LWater',150]],4,[['Coke',260],['LHOil',60]],180]
+            if usageFactory(pp3) : self.processes.append(pp3)
 
+# POWER PLANTS
+class DieselPowerPlant:
+    def __init__(self):
+        upgrade = input("\nYou are creating an DieselPowerPlant, which upgrade do you wish ? (0:None, 1:PetrolPowerPlant) ")
+        self.processes = []
+        pp0 = [False,0,[['LDiesel',25]],5,45]
+        if usagePlant(pp0) :self.processes.append(pp0)
+        if upgrade=='1':
+            pp1 = [False,0,[['LPetrol',50]],12,90]
+            if usagePlant(pp1) : self.processes.append(pp1)
+
+# TOOL FUNCTIONS
+def usageFactory(pp):
+    print('Do you want to use the process : ',end='')
+    for rr in pp[2] : print(str(rr[1])+rr[0],end='')
+    print(' -> ',end='')
+    for rr in pp[4] : print(str(rr[1])+rr[0],end='')
+    use = input('('+str(pp[5])+'s '+str(pp[3])+'MW) ? (y/n) ')
+    if use == 'y' : pp[0] = True
+    return use
+
+def usagePlant(pp):
+    print('Do you want to use the process : ',end='')
+    for rr in pp[2] : print(str(rr[1])+rr[0],end='')
+    print(' -> '+str(pp[3])+'MW',end='')
+    use = input('('+str(pp[4])+'s) ? (y/n) ')
+    if use == 'y' : pp[0] = True
+    return use
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy
-import matplotlib.pyplot
+import numpy as np
+import matplotlib.pyplot as plt
 from Buildings import *
 
 #======== Main Beginning ========
@@ -9,34 +9,58 @@ from Buildings import *
 
 # ==== Type 1 Simulation ====
 Res = {
+    'LDiesel' : 0,
     'Oil' : 0,
     'LOil' : 0,
+    'HOil' : 0,
+    'LHOil' : 0,
+    'EOil' : 0,
+    'LEOil' : 0,
     'Water' : 0,
     'LWater' : 0,
+    'Coal' : 0,
+    'Coke' : 0,
+    'Sulfur' : 0,
+    'Concrete' : 0,
+    'Petrol' : 0,
+    'LPetrol' : 0,
 }
-EPower = 0
 
-class OilWell:
-    def __init__(self,upgrade):
-        self.nbProcess = [1,3,3]
-        self.processes = [[True,0,[],0,[['Oil',1]],50]]
-        if upgrade==1:
-            self.processes.append([False,0,[],2,[['Oil',1]],26])
-            self.processes.append([False,0,[],2,[['LOil',75]],40]) 
-        if upgrade==2:
-            self.processes.append([False,0,[['LWater',25]],3,[['Oil',2]],40])
-            self.processes.append([False,0,[['LWater',25]],3,[['LOil',75]],30])
-
-
-duration = 110
-factories = [OilWell(0)]
+# Init Display
+DispRes = [[value] for key,value in Res.items()]
+ListElec = [0]
 
 # Start
+duration = int(input('\nHow many seconds for the simulation ? '))
+factories = []
+powerplants = []
+
+answer = 'x'
+while answer != 's':
+    answer = input('\nDo you want to add a building or resources or start the simulation ? (b/r/s) ')
+    if answer == 'b':
+        answer = input('Enter a building number : (0:OilWell, 1:CoalRefinery, 2:DieselPowerPlant) ')
+        if answer == '0' : 
+            oilwell = OilWell()
+            factories.append(oilwell)
+        if answer == '1' :
+            coalrafinery = CoalRefinery()
+            factories.append(coalrafinery)
+        if answer == '2' :
+            dieselpowerplant = DieselPowerPlant()
+            powerplants.append(dieselpowerplant)
+        
+    if answer == 'r':
+        print('\nResources are actually :')
+        print(Res)
+        mat,num = input('Type the exact name and the amount separated by a space : ').split()
+        Res[mat] += int(num)
 
 # Updates
 for t in range(1,duration+1):
-    for ff in factories:
-        for pp in ff.processes:
+    EPower = 0
+    for pp in powerplants:
+        for pp in pp.processes:
             if pp[0] == True:
                 if pp[1]>0:
                     pp[1] += 1
@@ -48,11 +72,65 @@ for t in range(1,duration+1):
                     if canBeStarted == True:
                         for rr in pp[2]:
                             Res[rr[0]] -= rr[1]
-                        pp[1] = 1   
+                        pp[1] = 1
+                if pp[1] == pp[4]:
+                    EPower += pp[3]
+                    pp[1] = 0
+                if pp[1] != 0:
+                    EPower += pp[3]
+            #print(pp)
+        
+    for ff in factories:
+        for pp in ff.processes:
+            if pp[0] == True:
+                if pp[1]>0:
+                    if EPower >= pp[3]:
+                        pp[1] += 1
+                        EPower -= pp[3]
+                if pp[1] == 0:
+                    canBeStarted = True
+                    for rr in pp[2]:
+                        if Res[rr[0]] < rr[1]:
+                            canBeStarted = False
+                    if canBeStarted == True and EPower >= pp[3]:
+                        for rr in pp[2]:
+                            Res[rr[0]] -= rr[1]
+                        pp[1] = 1
+                        EPower -= pp[3]
                 if pp[1] == pp[5]:
                     pp[1] = 0
                     for rr in pp [4]:
                         Res[rr[0]] += rr[1]
-            print(pp)
-    print(str(t)+' : '+str(Res))
+            #print(pp)
+
+    #print(str(t)+' : Res:'+str(Res)+'  Elec:'+str(EPower))
+    #Update Display Lists
+    i=0
+    for key,value in Res.items():
+        DispRes[i].append(value)
+        i += 1
+    ListElec.append(EPower)
+
+# End of Simulation
+print('\nRes:'+str(Res)+'  Elec:'+str(EPower))
+
+# Display
+fig,(ax1,ax2,ax3) = plt.subplots(3)
+
+i=0
+for key,value in Res.items():
+    if key[0] == 'L':
+        ax2.plot(range(0,duration+1),DispRes[i],label=key)
+    else:
+        ax1.plot(range(0,duration+1),DispRes[i],label=key)
+    i += 1
+
+ax1.grid()
+ax1.legend()
+ax2.grid()
+ax2.legend()
+ax3.plot(range(0,duration+1),ListElec,label='ElectricPower')
+ax3.grid()
+ax3.legend()
+plt.show()
 
